5dda19212ec2af777a98e8e119c1214e9e187b60
MagHermit <vladkakurin007@gmail.com>
2022-11-12 12:23:55 +0700

    integrate envs

--- samples/go/go.mod
+++ samples/go/go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/rpc v1.2.0
+	github.com/joho/godotenv v1.4.0
 	google.golang.org/protobuf v1.27.1
 )
--- samples/go/main.go
+++ samples/go/main.go
@@ -5,32 +5,53 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/rpc"
 	"github.com/gorilla/rpc/json"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 	"tw/core"
-	"tw/server"
+	serv "tw/server"
 )
 
 func main() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
 
-	server := new(server.JSONServer)
+	address, exists := os.LookupEnv("ADDRESS")
+
+	if !exists {
+		address = ""
+	}
+
+	port, exists := os.LookupEnv("PORT")
+
+	if !exists {
+		port = "8080"
+	}
+
+	server := new(serv.JSONServer)
 
 	fmt.Println("==> calling wallet core from go")
 
-	mn := "confirm bleak useless tail chalk destroy horn step bulb genuine attract split"
+	mn, exists := os.LookupEnv("MNEMONIC")
+	if !exists {
+		log.Fatal("can't find mnemonic")
+	}
 
 	fmt.Println("==> mnemonic is valid: ", core.IsMnemonicValid(mn))
 
 	server.MN = mn
 
-	port := ":8080"
-
 	s := rpc.NewServer()                                 // Create a new RPC server
 	s.RegisterCodec(json.NewCodec(), "application/json") // Register the type of data requested as JSON
-	s.RegisterService(server, "")                        // Register the service by creating a new JSON server
-
+	err := s.RegisterService(server, "")                 // Register the service by creating a new JSON server
+	if err != nil {
+		log.Fatal(err)
+	}
 	r := mux.NewRouter()
 	r.Handle("/api/v1/sign_transaction", s)
-	log.Fatal(http.ListenAndServe(port, r))
+	log.Fatal(http.ListenAndServe(address+":"+port, r))
 
 }

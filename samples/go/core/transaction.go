7d9c8a56d63fdb97c55e84e71eac5294b16c9593
MagHermit <vladkakurin007@gmail.com>
2022-11-23 01:16:03 +0700

    add json-signing method

--- samples/go/core/transaction.go
+++ samples/go/core/transaction.go
@@ -7,9 +7,9 @@ package core
 import "C"
 
 import (
-	"tw/types"
-
+	"errors"
 	"google.golang.org/protobuf/proto"
+	"tw/types"
 )
 
 func CreateSignedTx(inputData proto.Message, ct CoinType, outputData proto.Message) error {
@@ -25,4 +25,25 @@ func CreateSignedTx(inputData proto.Message, ct CoinType, outputData proto.Messa
 		return err
 	}
 	return nil
+}
+
+func CreateSignedTxFromJson(json []byte, key string, ct CoinType) (string, error) {
+	supported := C.TWAnySignerSupportsJSON(C.enum_TWCoinType(ct))
+	if !supported {
+		return "", errors.New("json signing not supported for given coin")
+	}
+
+	ijson := types.TWStringCreateWithGoString(string(json))
+	defer C.TWStringDelete(ijson)
+
+	ikey := types.TWDataCreateWithGoHex(key)
+	defer C.TWDataDelete(ikey)
+
+	odata := C.TWAnySignerSignJSON(ijson, ikey, C.enum_TWCoinType(ct))
+	defer C.TWStringDelete(odata)
+
+	res := types.TWStringGoString(odata)
+
+	return res, nil
+
 }
